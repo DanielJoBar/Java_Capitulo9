8184dd6f4c9112190aa3c6a8bc5fd5868b3dbf6a
DanielJoBar <115071984+DanielJoBar@users.noreply.github.com>
2023-01-17 14:38:15 +0100

    Add files via upload

---
+++ Ejercicio5/Pizza.java
@@ -0,0 +1,11 @@
+package Ejercicio5;
+public class Pizza {
+    //Atributos:
+    protected String tamaño;
+    protected String tipo;
+    protected String estado;
+    Pizza(String tipo,String tamaño){
+        this.tipo = tipo;
+        this.tamaño = tamaño;
+    }
+}
